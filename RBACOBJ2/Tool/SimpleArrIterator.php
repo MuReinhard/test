ba48b9b3bb9e0bbd0a00bdc6435e93c175758d0c
gonghaichao <2607572176@qq.com>
2016-08-26 20:00:17 +0800

    // RBAC-第二项目-v0.2-日常提交-简单数组迭代器

---
+++ RBACOBJ2/Tool/SimpleArrIterator.php
@@ -0,0 +1,57 @@
+<?php
+namespace Tool;
+
+/**
+ * @class SimpleIterator
+ * @author ShiO
+ */
+class SimpleArrIterator extends IIterator{
+    public $data;
+
+    public function __construct(array $data) {
+        $this->data = $data;
+    }
+
+    /**
+     * @author ShiO
+     * @param $value
+     */
+    public function add($value) {
+        $this->data[] = $value;
+    }
+
+    /**
+     * @author ShiO
+     */
+    public function del() {
+        array_pop($this->data);
+    }
+
+    /**
+     * @author ShiO
+     * @return mixed
+     */
+    public function first() {
+        return reset($this->data);
+    }
+
+    /**
+     * @author ShiO
+     * @return mixed
+     */
+    public function last() {
+        return end($this->data);
+    }
+
+    /**
+     * @author ShiO
+     * @return mixed
+     */
+    public function next() {
+        return next($this->data);
+    }
+
+    public function all() {
+        return $this->data;
+    }
+}
